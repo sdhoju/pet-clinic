1a6a893cca25eeb4ea51e43a33ca6a3187ae3744
sdhoju <sdhoju@go.olemiss.edu>
2019-01-15 01:12:30 -0600

    add POJOs Closes #2.

---
+++ src/main/java/samee/springframework/petclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package samee.springframework.petclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/samee/springframework/petclinic/model/PetType.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package samee.springframework.petclinic.model;
+
+/**
+ * @author Sameer
+ *
+ */
+public class PetType {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
